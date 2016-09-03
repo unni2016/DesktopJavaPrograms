509b8e813a69a40c13720b1f4eeab60ebcaeaa50
unni2016 <unni2b4u@yahoo.com>
2016-09-03 23:00:41 +0530

    Add files via upload

---
+++ helloworldprogram.java
@@ -0,0 +1,10 @@
+package AccionTalendExamples;
+
+public class helloworldprogram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello World, how are you doing!");
+	}
+
+}
